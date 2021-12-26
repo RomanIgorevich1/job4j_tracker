8e35933be3e91658430328ec2488bb2f90bc62ec
RomanIgorevich1 <votmoyapochta1@bk.ru>
2021-12-26 14:14:39 +0300

    Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,29 @@
+package ru.job4j.oop;
+
+public class Error {
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String massage) {
+        this.active = active;
+        this.status = status;
+        this.massage = massage;
+    }
+
+    private boolean active;
+    private int status;
+    private String massage;
+
+    public void info() {
+        System.out.println("2 + 2 = 4 " + active);
+        System.out.println("100 + 100 =  " + status);
+        System.out.println("Hello " + massage);
+    }
+
+    public static void main(String[] args) {
+        Error err = new Error();
+        Error err1 = new Error(true, 200, "World!");
+        err.info();
+        err1.info();
+    }
+}
