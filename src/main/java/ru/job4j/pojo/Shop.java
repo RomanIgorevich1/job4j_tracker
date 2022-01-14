093fc9d29cc9b76af7c03b191e5e4829da39d41f
RomanIgorevich1 <votmoyapochta1@bk.ru>
2022-01-14 09:13:21 +0300

    Массив с пустыми ячейками [#176470 #250692]

--- src/main/java/ru/job4j/pojo/Shop.java
+++ src/main/java/ru/job4j/pojo/Shop.java
@@ -2,21 +2,21 @@ package ru.job4j.pojo;
 
 public class Shop {
     public static void main(String[] args) {
-        Product[] prods = new Product[5];
-        prods[0] = new Product("Milk", 10);
-        prods[1] = new Product("Bread", 13);
-        prods[2] = new Product("Egg", 9);
-        for (Product prod : prods) {
-            if (prod != null) {
-                System.out.println(prod.getName());
+        Product[] products = new Product[5];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 13);
+        products[2] = new Product("Egg", 9);
+        for (Product product : products) {
+            if (product != null) {
+                System.out.println(product.getName());
             }
         }
     }
 
-    public static int indexOfNull(Product[] prods) {
-        for (int i = 0; i < prods.length; i++) {
-            Product prod = prods[i];
-            if (prod == null) {
+    public static int indexOfNull(Product[] products) {
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product == null) {
                return i;
             }
         } return -1;
--- src/test/java/ru/job4j/pojo/ShopTest.java
+++ src/test/java/ru/job4j/pojo/ShopTest.java
@@ -9,29 +9,27 @@ public class ShopTest {
 
     @Test
     public void whenLastNull() {
-        Product[] prods = new Product[5];
-        prods[0] = new Product("Milk", 10);
-        prods[1] = new Product("Bread", 13);
-        prods[2] = new Product("Egg", 9);
-        int rsl = Shop.indexOfNull(prods);
+        Product[] products = new Product[5];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 13);
+        products[2] = new Product("Egg", 9);
+        int rsl = Shop.indexOfNull(products);
         assertThat(rsl, is(3));
     }
 
     @Test
     public void whenFirstNull() {
-        Product[] prods = new Product[5];
-        prods[1] = new Product("Milk", 10);
-        int rsl = Shop.indexOfNull(prods);
+        Product[] products = new Product[5];
+        products[1] = new Product("Milk", 10);
+        int rsl = Shop.indexOfNull(products);
         assertThat(rsl, is(0));
     }
 
     @Test
     public void whenHasNotNull() {
-        Product[] prods = new Product[3];
-        prods[0] = new Product("Milk", 10);
-        prods[1] = new Product("Bread", 13);
-        prods[2] = new Product("Egg", 9);
-        int rsl = Shop.indexOfNull(prods);
+        Product[] products = new Product[1];
+        products[0] = new Product("Milk", 10);
+        int rsl = Shop.indexOfNull(products);
         assertThat(rsl, is(-1));
     }
 }
