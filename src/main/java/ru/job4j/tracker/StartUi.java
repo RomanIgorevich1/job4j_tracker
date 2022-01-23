28174161f426f50a7d87bbf7265fc5dc9b1c57d2
RomanIgorevich1 <votmoyapochta1@bk.ru>
2022-01-23 13:30:12 +0300

    Реализация класса StartUI. Удаление заявки [#500747]

--- src/main/java/ru/job4j/tracker/StartUi.java
+++ src/main/java/ru/job4j/tracker/StartUi.java
@@ -1,5 +1,6 @@
 package ru.job4j.tracker;
 
+
 import java.util.Scanner;
 
 public class StartUi {
@@ -38,6 +39,15 @@ public class StartUi {
                     System.out.println("Заявка изменена успешно.");
                 } else {
                     System.out.println("Ошибка замены заявки.");
+                }
+            } else if (select == 3) {
+                System.out.println("=== Delete item ===");
+                System.out.print("Enter id: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                if (tracker.delete(id)) {
+                    System.out.println("Заявка удалена успешно.");
+                } else {
+                    System.out.println("Ошибка удаления заявки.");
                 }
             } else if (select == 6) {
                 System.out.println("Пользователь выбрал " + select);
