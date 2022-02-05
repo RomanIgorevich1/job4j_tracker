96faf137cc30560efbd7b968f87e6567f609b284
votmo <votmoyapochta1@bk.ru>
2022-02-05 20:34:28 +0300

    final [#334073 #260455]

--- src/main/java/ru/job4j/ooa/Airbus.java
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -23,7 +23,7 @@ public final class Airbus extends Aircraft {
     }
 
     public void printCountEngine() {
-        System.out.println("Кол - во двигателей равно " + (getName().equals("A380") ? 4 : COUNT_ENGINE));
+        System.out.println("Кол - во двигателей равно " + (("A380").equals(getName()) ? 4 : COUNT_ENGINE));
     }
 
     @Override
