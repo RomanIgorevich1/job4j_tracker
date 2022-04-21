7fb66f6191e8dcbf0f897dd2668c41cc2cf52d89
votmo <votmoyapochta1@bk.ru>
2022-04-21 21:31:11 +0300

    Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,23 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public static boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] duplicate = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String str : origin) {
+            check.add(str);
+        }
+        for (String str : duplicate) {
+            if (check.contains(str)) {
+                break;
+            } else {
+                rsl = false;
+            }
+        }
+        return rsl;
+    }
+}
