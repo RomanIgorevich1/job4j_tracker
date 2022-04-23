a87454b0dcf79a9fd0d20014756176eaa92981a9
votmo <votmoyapochta1@bk.ru>
2022-04-23 13:04:28 +0300

    Преобразования List в Map. [#10093]

---
+++ src/main/java/ru/job4j/collection/Order.java
@@ -0,0 +1,41 @@
+package ru.job4j.collection;
+
+import java.util.Objects;
+
+public class Order {
+    private String number;
+    private String name;
+
+    Order(String number, String name) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, number);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Order order = (Order) obj;
+        return Objects.equals(number, order.number)
+                && Objects.equals(name, order.name);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" + "number='" + number + '\''
+                + ", name='" + name + '\'' + "}";
+    }
+}
