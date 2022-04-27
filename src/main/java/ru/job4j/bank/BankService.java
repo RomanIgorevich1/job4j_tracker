bd84d9456967377d8434a3551bbec81d34ead21b
votmo <votmoyapochta1@bk.ru>
2022-04-27 18:28:22 +0300

    Банковские переводы [#10038]

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,53 @@
+package ru.job4j.bank;
+
+import java.util.*;
+
+public class BankService {
+    private final Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.putIfAbsent(user, new ArrayList<Account>());
+    }
+
+    public void adAccount(String passport, Account account) {
+        User client = findByPassport(passport);
+        List<Account> list = users.get(client);
+        if (!list.contains(account)) {
+            list.add(account);
+        }
+    }
+
+    public User findByPassport(String passport) {
+        for (User client : users.keySet()) {
+            if (client.getPassport().equals(passport)) {
+                return client;
+            }
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        User client = findByPassport(passport);
+        List<Account> list = users.get(client);
+        if (client != null) {
+        for (Account acc : list) {
+            if (acc.getRequisite().equals(requisite)) {
+                return acc;
+            }
+        }
+        }
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        Account srcAcc = findByRequisite(srcPassport, srcRequisite);
+        Account destAcc = findByRequisite(destPassport, destRequisite);
+        boolean rsl = false;
+        if (srcAcc != null && destAcc != null && srcAcc.getBalance() > 0) {
+            destAcc.setBalance(destAcc.getBalance() + amount);
+            rsl = true;
+        }
+        return rsl;
+    }
+}
---
+++ src/main/java/ru/job4j/bank/User.java
@@ -0,0 +1,46 @@
+package ru.job4j.bank;
+
+import java.util.Objects;
+
+public class User {
+    private String username;
+    private String passport;
+
+    public User(String username, String passport) {
+        this.username = username;
+        this.passport = passport;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassport() {
+        return passport;
+    }
+
+    public void setPassport(String passport) {
+        this.passport = passport;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(passport);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        User user = (User) obj;
+        return Objects.equals(passport, user.passport);
+    }
+}
