81e4cf327299cbb80e291666fe3756dcffabfba6
votmo <votmoyapochta1@bk.ru>
2022-06-28 16:43:50 +0300

    Фильтрация учеников [#110058]

---
+++ src/main/java/ru/job4j/stream/Student.java
@@ -0,0 +1,39 @@
+package ru.job4j.stream;
+
+import java.util.Objects;
+
+public class Student {
+    private String surname;
+    private int score;
+
+    public Student(String surname, int score) {
+        this.surname = surname;
+        this.score = score;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(surname, score);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Student student = (Student) obj;
+        return score == student.getScore()
+                && Objects.equals(surname, student.surname);
+    }
+}
