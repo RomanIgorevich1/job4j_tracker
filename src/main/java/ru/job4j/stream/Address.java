0ce03fd16d430f0b37c5f13814bda60585315004
votmo <votmoyapochta1@bk.ru>
2022-06-28 17:17:00 +0300

    Список адресов [#110062]

---
+++ src/main/java/ru/job4j/stream/Address.java
@@ -0,0 +1,37 @@
+package ru.job4j.stream;
+
+import java.util.Objects;
+
+public class Address {
+    private String city;
+    private String street;
+    private int home;
+    private int apartment;
+
+    public Address(String city, String street, int home, int apartment) {
+        this.city = city;
+        this.street = street;
+        this.home = home;
+        this.apartment = apartment;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, apartment, home);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != getClass()) {
+            return false;
+        }
+        Address address = (Address) obj;
+        return home == address.home
+                && apartment == address.apartment
+                && Objects.equals(city, address.city)
+                && Objects.equals(street, address.street);
+    }
+}
---
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ProfilesTest {
+
+    @Test
+    public void whenTestMethodCollect() {
+        Address one = new Address("City1", "Street1", 1, 1);
+        Address two = new Address("City2", "Street2", 2, 2);
+        Address three = new Address("City3", "Street3", 3, 3);
+        List<Profile> profileList = Arrays.asList(
+                new Profile(one),
+                new Profile(two),
+                new Profile(three));
+        List<Address> result = Profiles.collect(profileList);
+        List<Address> expected = Arrays.asList(one, two, three);
+        assertEquals(result, expected);
+    }
+}
