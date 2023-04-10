7e75215a872edaf1f02256a51d96e5dd9ffba9a5
votmo <votmoyapochta1@bk.ru>
2023-04-10 20:44:27 +0300

    Трекер SQL [#1734 #406157]

--- src/main/java/ru/job4j/tracker/SqlTracker.java
+++ src/main/java/ru/job4j/tracker/SqlTracker.java
@@ -1,4 +1,5 @@
 package ru.job4j.tracker;
+
 import java.io.InputStream;
 import java.sql.*;
 import java.util.ArrayList;
@@ -38,12 +39,11 @@ public class SqlTracker implements Store {
     }
 
     @Override
-    public Item add(Item item)  {
+    public Item add(Item item) {
         try (PreparedStatement statement = connection.prepareStatement(
                 "insert into items(name, created) values (?, ?)", Statement.RETURN_GENERATED_KEYS)) {
             statement.setString(1, item.getName());
-            Timestamp times = Timestamp.valueOf(item.getCreated());
-            statement.setTimestamp(2, times);
+            statement.setTimestamp(2, Timestamp.valueOf(item.getCreated()));
             statement.execute();
             try (ResultSet generatedKey = statement.getGeneratedKeys()) {
                 if (generatedKey.next()) {
@@ -57,14 +57,13 @@ public class SqlTracker implements Store {
     }
 
     @Override
-    public boolean replace(int id, Item item)  {
+    public boolean replace(int id, Item item) {
         boolean result = false;
         try (PreparedStatement statement = connection.prepareStatement(
                 "update items set name = ?, created = ? where id = ?"
         )) {
             statement.setString(1, item.getName());
-            Timestamp times = Timestamp.valueOf(item.getCreated());
-            statement.setTimestamp(2, times);
+            statement.setTimestamp(2, Timestamp.valueOf(item.getCreated()));
             statement.setInt(3, id);
             result = statement.executeUpdate() > 0;
         } catch (SQLException exception) {
@@ -93,6 +92,7 @@ public class SqlTracker implements Store {
         try (PreparedStatement statement = connection.prepareStatement(
                 "select * from items"
         )) {
+            statement.execute();
             try (ResultSet resultSet = statement.getResultSet()) {
                 while (resultSet.next()) {
                     itemList.add(new Item(resultSet.getInt("id"),
@@ -115,7 +115,8 @@ public class SqlTracker implements Store {
             statement.execute();
             try (ResultSet resultSet = statement.getResultSet()) {
                 while (resultSet.next()) {
-                    itemList.add(new Item(resultSet.getString("name")));
+                    itemList.add(new Item(resultSet.getInt("id"),
+                            resultSet.getString("name")));
                 }
             }
         } catch (SQLException exception) {
@@ -133,7 +134,9 @@ public class SqlTracker implements Store {
             statement.setInt(1, id);
             statement.execute();
             try (ResultSet resultSet = statement.getResultSet()) {
-               newItem = new Item(resultSet.getInt("id"), resultSet.getString("name"));
+                if (resultSet.next()) {
+                    newItem = new Item(resultSet.getInt("id"), resultSet.getString("name"));
+                }
             }
         } catch (SQLException exception) {
             exception.printStackTrace();
